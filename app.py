c474531d8a2a0f8573fb7d88b00dce943598af57
Anand Suresh <wwwanandsuresh@gmail.com>
2019-10-20 21:51:38 +0530

    added basic flask app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import webbrowser
 
 app = Flask(__name__)
 
@@ -7,9 +8,18 @@ def main():
     return render_template('index.html')
 
     
-@app.route('/url',methods-['POST','GET'])
+@app.route('/url',methods=['POST','GET'])
 def url():
     if request.method == 'POST':
         resp_json = request.get_json()
-        f =  resp_json['text']
+        global f
+        f =  resp_json['text'] #link
         print(f)
+
+        return 1
+
+if __name__ == '__main__':
+    app.run(debug=False)
+    webbrowser.open('http://127.0.0.1:5000')
+
+        
