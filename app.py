3d65f3549f009dc41b39832ad1185ad2890db482
Anand Suresh <wwwanandsuresh@gmail.com>
2019-10-21 03:42:40 +0530

    pushed templates

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import webbrowser
+import webbrowser, json
 
 app = Flask(__name__)
 
@@ -12,14 +12,13 @@ def main():
 def url():
     if request.method == 'POST':
         resp_json = request.get_json()
-        global f
         f =  resp_json['text'] #link
         print(f)
 
-        return 1
+        return json.dumps({"response": "please wait"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=False)
     webbrowser.open('http://127.0.0.1:5000')
+    app.run(debug=False)
 
         
