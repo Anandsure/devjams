52e4ead6b854be5015b3a6926c90d7cf2ec3733c
Anand Suresh <wwwanandsuresh@gmail.com>
2019-10-20 21:44:50 +0530

    added flask app

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 @app.route('/',methods=['POST','GET'])
 def main():
+    return render_template('index.html')
+
+    
+@app.route('/url',methods-['POST','GET'])
+def url():
     if request.method == 'POST':
         resp_json = request.get_json()
         f =  resp_json['text']
         print(f)
-    return render_template('index.html')
